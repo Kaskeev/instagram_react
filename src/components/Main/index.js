c1f50717886bec79d583f3686ce1d23b956a6240
Kaskeev <62796577+Kaskeev@users.noreply.github.com>
2021-01-09 00:14:48 +0600

    added right content

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,18 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import Card from '../Card/Card'
 import Slider from '../Slider'
 import styles from "./Main.css"
 
 export default function Main() {
+
+    const [cards] = useState([1, 2, 3])
     return (
         <div>
             <div className="main">
-                <div>
+                <div className="main__wrapper">
                 <Slider/>
-                <Card />
+                <div className="main__cardWrapper">
+                  {cards.map(() => {
+                      return(
+                          <Card/>
+                      )
+                  })}
+                </div>
                 </div>
                 <div className="main__reccomendation">
-                    
+                <div className="main__header">
+                    <div className = "main__UserLogo"></div>
+                        <div className="main__userDate">
+                        <div className = "main__userName">Kaskeev17</div>
+                        <div className="main__userPosition">Kyrgyzstan, Bishkek</div>
+                        </div>
+                        <div className="main__reloc">
+                            Переключиться
+                        </div>
+                </div>
                 </div>
             </div>
         </div>
